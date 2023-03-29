e5251143ef4b5e9f7dd860f52dfee47097304e33
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2023-03-29 14:04:12 +0800

    Graalvm native compile passed. Clear.

--- src/main/java/cc/xfl12345/app/Main.java
+++ src/main/java/cc/xfl12345/app/Main.java
@@ -1,36 +1,14 @@
 package cc.xfl12345.app;
 
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigurationProperties
 @SpringBootApplication
-@ComponentScan("cc.xfl12345.web")
-@ComponentScan("cc.xfl12345.config")
 public class Main {
-    private static ConfigurableApplicationContext context;
-
-    public static int restartCount = 0;
-
     public static void main(String[] args) {
-        context = SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
-    public static void restart() {
-        ApplicationArguments args = context.getBean(ApplicationArguments.class);
-
-        Thread thread = new Thread(() -> {
-            context.close();
-            context = SpringApplication.run(Main.class, args.getSourceArgs());
-        });
-
-        restartCount += 1;
-        thread.setDaemon(false);
-        thread.setName("restart-" + restartCount);
-        thread.start();
-    }
 }
