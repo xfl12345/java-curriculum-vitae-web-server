0e4a679b5ffae24779e77eb3e7bbe7c25f327740
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2024-07-03 22:52:44 +0800

    workflow debug x6

--- setup_gradle_proxy_from_environment.sh
+++ setup_gradle_proxy_from_environment.sh
@@ -6,17 +6,25 @@ if [[ ! -e $HOME/.gradle/gradle.properties ]]; then
 fi
 
 if [[ x"${HTTPS_PROXY}" != "x" ]]; then
-    echo "HTTPS_PROXY found. HTTPS_PROXY=${HTTPS_PROXY}"
-    cat $HOME/.gradle/gradle.properties | grep -v "systemProp\.https\." > $HOME/.gradle/gradle.properties
+    echo "HTTPS_PROXY found. HTTPS_PROXY=${HTTPS_PROXY}   Adding..."
+    cat $HOME/.gradle/gradle.properties | grep -v "systemProp\.https\.proxy" > $HOME/.gradle/gradle.properties
     https_proxy_host_and_port=$(echo $HTTPS_PROXY | sed "s#https://##g" | sed "s#http://##g")
     echo "systemProp.https.proxyHost=$(echo $https_proxy_host_and_port | cut -d':' -f1)" >> $HOME/.gradle/gradle.properties
     echo "systemProp.https.proxyPort=$(echo $https_proxy_host_and_port | cut -d':' -f2)" >> $HOME/.gradle/gradle.properties
+    echo "Done"
+    echo "cat $HOME/.gradle/gradle.properties >>>"
+    cat $HOME/.gradle/gradle.properties
+    echo "<<<"
 fi
 
 if [[ x"${HTTP_PROXY}" != "x" ]]; then
-    echo "HTTP_PROXY found. HTTP_PROXY=${HTTPS_PROXY}"
-    cat $HOME/.gradle/gradle.properties | grep -v "systemProp\.http\." > $HOME/.gradle/gradle.properties
+    echo "HTTP_PROXY found. HTTP_PROXY=${HTTPS_PROXY}   Adding..."
+    cat $HOME/.gradle/gradle.properties | grep -v "systemProp\.http\.proxy" > $HOME/.gradle/gradle.properties
     http_proxy_host_and_port=$(echo $HTTP_PROXY | sed "s#http://##g")
     echo "systemProp.http.proxyHost=$(echo $http_proxy_host_and_port | cut -d':' -f1)" >> $HOME/.gradle/gradle.properties
     echo "systemProp.http.proxyPort=$(echo $http_proxy_host_and_port | cut -d':' -f2)" >> $HOME/.gradle/gradle.properties
+    echo "Done"
+    echo "cat $HOME/.gradle/gradle.properties >>>"
+    cat $HOME/.gradle/gradle.properties
+    echo "<<<"
 fi
