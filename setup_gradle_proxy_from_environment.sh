3eea491a40d196dc7022b22754921e2a3cf26d1a
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2024-07-03 22:45:25 +0800

    workflow debug x5

--- setup_gradle_proxy_from_environment.sh
+++ setup_gradle_proxy_from_environment.sh
@@ -6,6 +6,7 @@ if [[ ! -e $HOME/.gradle/gradle.properties ]]; then
 fi
 
 if [[ x"${HTTPS_PROXY}" != "x" ]]; then
+    echo "HTTPS_PROXY found. HTTPS_PROXY=${HTTPS_PROXY}"
     cat $HOME/.gradle/gradle.properties | grep -v "systemProp\.https\." > $HOME/.gradle/gradle.properties
     https_proxy_host_and_port=$(echo $HTTPS_PROXY | sed "s#https://##g" | sed "s#http://##g")
     echo "systemProp.https.proxyHost=$(echo $https_proxy_host_and_port | cut -d':' -f1)" >> $HOME/.gradle/gradle.properties
@@ -13,6 +14,7 @@ if [[ x"${HTTPS_PROXY}" != "x" ]]; then
 fi
 
 if [[ x"${HTTP_PROXY}" != "x" ]]; then
+    echo "HTTP_PROXY found. HTTP_PROXY=${HTTPS_PROXY}"
     cat $HOME/.gradle/gradle.properties | grep -v "systemProp\.http\." > $HOME/.gradle/gradle.properties
     http_proxy_host_and_port=$(echo $HTTP_PROXY | sed "s#http://##g")
     echo "systemProp.http.proxyHost=$(echo $http_proxy_host_and_port | cut -d':' -f1)" >> $HOME/.gradle/gradle.properties
